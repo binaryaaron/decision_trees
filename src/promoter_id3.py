a70d82ac533e27c30e27f40710fca73803a1c58a
Aaron Gonzales <agonzales@cs.unm.edu>
2015-01-22 00:03:06 -0700

    inital src update; reads data file and that is it

---
+++ src/promoter_id3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+""" 
+promoter_id3.py is the part that holds the program together.
+It is a part of the CS529 Machine Learning project
+1 submission for Aaron Gonzales.
+"""
+
+import sys
+import argparse
+import csv 
+import id3
+import process_data
+
+__author__ = "Aaron Gonzales"
+__copyright__ = "GPL"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Aaron Gonzales"
+__email__ = "agonzales@cs.unm.edu"
+__status__ = "Development"
+__version__ = "0.1"
+__date__ = "2015-01-20"
+
+
+
