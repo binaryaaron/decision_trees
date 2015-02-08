b6d08917046d02680a73e6da6a3d98f18382e53f
Aaron Gonzales <agonzales@cs.unm.edu>
2015-02-08 15:21:34 -0700

    cleaned up syntax and stray commented lines

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import division
 """ 
-process_data.py is the helper file that will read DNA promoter data
-and create objects from that. It is a part of the CS529 Machine Learning project
-1 submission for Aaron Gonzales.
+metrics.py holds the functions for calculating information gain and statistics
+for the decision tree.
+It is a part of the CS529 Machine Learning project 1 submission for Aaron Gonzales.
 """
 
 import math
@@ -20,19 +20,13 @@ def entropy(base, n):
   """ Calculates the entropy of a set of attributes
   Attributes:
     base - Feature
+  Return (float): entropy
   """
-  # convenience
-
-  # base case, need to chekc this
-  # if base[2] <=1  or base[0]  <=1 or base[1]  <= 1:
-  if base[2] <=1: 
+  # base case, need to check this
+  if base[2] <=1:
     return 0
-
   p_pos = base[0]/base[2]
   p_neg = base[1]/base[2]
-  # print math.log(p, 2)
-  # print "p pos: %f" % p_pos
-  # print "p neg: %f" % p_neg
   if p_pos != 0:
     entpos = -1* (p_pos * math.log(p_pos, 2) )
   else:
@@ -41,13 +35,7 @@ def entropy(base, n):
     entneg = -1* (p_neg * math.log(p_neg, 2) )
   else:
     entneg = 0
-  # print "ent pos: %f" % entneg
-  # print "ent neg: %f" % entneg
-  ent = entpos + entneg
-
-  # ent = (p_pos * math.log(p_pos, 2) ) - (p_neg * math.log(p_neg, 2))
-  # print "entropy: %f " % ent
-  return ent
+  return entpos + entneg
 
 
 def info_gain(f):
@@ -72,7 +60,7 @@ def info_gain(f):
   for base in f.bases:
     info = base[2]/f.n * entropy(base, f.n)
     info_sum += info
-    if DEBUG: 
+    if DEBUG:
       print 'for loop in info gain: done'
       print base, info, info_sum
 
@@ -83,14 +71,17 @@ def info_gain(f):
 def chi_squared(f, threshold=95):
   """
   Performs the chi-squared test needed for testing effectiveness of a split.
+  Args:
+    f (Feature): the current feature we are testing
+    threshold (int): optional value that gives us the threshold to use
   """
   # sum  (promoters_ichild - expected_promoters_ichild)^2  + (non-p_i - # exp_non_pi)^2
   #   -----------------------------------------------------  -------------------------
   #       exp_promoters_ichild                                      exp_non_pi
-    # feature reference
-    # n : size of the feature
-    # a : tuple of base pair a (n_pos, n_neg, n_tot)
-    # recall that a base is a 4tuple with 
+  # feature reference
+  # n : size of the feature
+  # a : tuple of base pair a (n_pos, n_neg, n_tot)
+  # recall that a base is a 4tuple with
   chisq = float(0)
   # degrees of freedom vary if a base is empty
   dof = 3
@@ -101,40 +92,43 @@ def chi_squared(f, threshold=95):
         2 : 5.991,
         3 : 9.837,
         4 : 11.668
-        }, 
+        },
       99 : {
         1 : 6.635,
         2 : 9.210,
         3 : 11.345,
-        4 : 13.227 
+        4 : 13.227
+        },
+      0 : {
+        1 : 0.001,
+        2 : 0.001,
+        3 : 0.001,
+        4 : 0.001
         }
       }
-
-  # print str(f)
+  chi_pos = 0
+  chi_neg = 0
   for base in f.bases:
     base_pos = base[ 0 ]
     base_neg = base[ 1 ]
     base_tot = base[ 2 ]
-    # print ' this base is : %s ' % str(base)
     if base_tot == 0:
       dof -= 1
-      continue
+    #  continue
     expected_pos = base_tot * (f.pos/f.n)
     expected_neg = base_tot * (f.neg/f.n)
-    chi_pos =((base_pos - expected_pos)**2) / expected_pos
-    chi_neg = ((base_neg - expected_neg)**2) / expected_neg
+    if expected_pos == 0:
+      chi_pos = 0
+    elif expected_neg == 0:
+      chi_neg = 0
+    else:
+      chi_pos =((base_pos - expected_pos)**2) / expected_pos
+      chi_neg = ((base_neg - expected_neg)**2) / expected_neg
     chisq += chi_pos + chi_neg
-  # print ("chi_squared minium value for dof: %d and threshold %d: %f " %
-      # (dof, threshold, chisq_mins[threshold][dof]) )
-  # print "computed chi_squared value : %d" % chisq
+  # print ("chi_squared minium value for dof: %f and threshold %d: %f " %
+  #     (dof, threshold, chisq_mins[threshold][dof]) )
+  # print "computed chi_squared value : %f" % chisq
 
   if dof == 0:
     dof = 1
   return chisq > chisq_mins[threshold][dof]
-
-
-
-
-
-
-
